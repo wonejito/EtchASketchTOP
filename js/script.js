7110ff15d37006c1a9c8ee4370e8c1de61244f27
wonejito <30444850+wonejito@users.noreply.github.com>
2023-02-08 10:26:01 -0400

    Im lost.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
-let numeroDeCuadros = 18;
+let numeroDeCuadros = 8;
+let clickme = document.querySelector(".clickme");
+
+clickme.addEventListener("click", function () {
+	numeroDeCuadros = parseInt(prompt("Di: "));
+});
+
 let contenedor = document.querySelector("#contenedor");
 contenedor.style.cssText = `grid-template-columns: repeat(${numeroDeCuadros}, 1fr); grid-template-rows: repeat(${numeroDeCuadros}, 1fr);`;
 let titulo = document.querySelector("#title");
 
-for (i = 0; i < numeroDeCuadros * numeroDeCuadros; i++) {
-	let cubo = document.createElement("div");
-	cubo.className = "square";
-	cubo.style.backgroundColor = "grey";
-	cubo.style.width = 256 / numeroDeCuadros;
-	cubo.style.height = 256 / numeroDeCuadros;
-	cubo.style.border = "1px solid yellow";
-	contenedor.appendChild(cubo);
+function defaultt() {
+	for (i = 0; i < numeroDeCuadros * numeroDeCuadros; i++) {
+		let cubo = document.createElement("div");
+		cubo.className = "square";
+		cubo.style.backgroundColor = "grey";
+		cubo.style.width = 256 / numeroDeCuadros;
+		cubo.style.height = 256 / numeroDeCuadros;
+		cubo.style.border = "1px solid yellow";
+		contenedor.appendChild(cubo);
+	}
 }
+defaultt();
+
 let cubos = document.querySelectorAll(".square");
 
 cubos.forEach((cubito) => {
@@ -19,28 +29,3 @@ cubos.forEach((cubito) => {
 		this.style.backgroundColor = "yellow";
 	});
 });
-
-// let divs = document.createElement("div");
-// divs.style.cssText = "background-color:blue;height:100px;width:100px;";
-
-// principalgrid.appendChild(divs);
-
-// title.addEventListener("click", function () {
-//   let divs = document.createElement("div");
-//   divs.style.cssText = "background-color:blue;height:100px;width:100px;";
-
-//   principalgrid.appendChild(divs);
-// });
-
-//intento de crear una funcion que lo genere automaticamente
-// function createElements(number) {
-//     for (i = 0; i < number; i++) {
-//         let divs = document.createElement("div");
-//         divs.style.cssText =
-//             "background-color:blue;height:32px;width:32px; border: 1px solid green";
-//         divs.className = "claseDiv";
-
-//         principalgrid.appendChild(divs);
-//     }
-// }
-// createElements(16);
