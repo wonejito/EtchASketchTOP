b501caaaceab1ef9d2b3605ac486c2897b86c268
wonejito <30444850+wonejito@users.noreply.github.com>
2023-02-12 12:28:40 -0400

    Done. Need to come back an re-read it again to undertand what my own code does xD

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@ let clickme = document.querySelector(".clickme");
 let contenedor = document.querySelector("#contenedor");
 
 function defaultt() {
-	let numeroDeCuadros = 14;
+	let numeroDeCuadros = 24;
 	for (i = 0; i < numeroDeCuadros * numeroDeCuadros; i++) {
 		let cubo = document.createElement("div");
 		cubo.className = "square";
-		cubo.style.backgroundColor = "grey";
+		cubo.style.backgroundColor = "white";
 		cubo.style.width = 256 / numeroDeCuadros;
 		cubo.style.height = 256 / numeroDeCuadros;
-		cubo.style.border = "1px solid yellow";
+		// cubo.style.border = "1px solid white";
 		contenedor.style.cssText = `grid-template-columns: repeat(${numeroDeCuadros}, 1fr); grid-template-rows: repeat(${numeroDeCuadros}, 1fr);`;
 		contenedor.appendChild(cubo);
 	}
@@ -20,7 +20,7 @@ let cubos = document.querySelectorAll(".square");
 function addListeners(cubos) {
 	cubos.forEach((cubito) => {
 		cubito.addEventListener("mouseover", function () {
-			this.style.backgroundColor = "yellow";
+			this.style.backgroundColor = "black";
 		});
 	});
 }
@@ -39,10 +39,10 @@ function costum() {
 	for (j = 0; j < numeroDeCuadros * numeroDeCuadros; j++) {
 		let cubo = document.createElement("div");
 		cubo.className = "square";
-		cubo.style.backgroundColor = "grey";
+		cubo.style.backgroundColor = "white";
 		cubo.style.width = 256 / numeroDeCuadros;
 		cubo.style.height = 256 / numeroDeCuadros;
-		cubo.style.border = "1px solid yellow";
+		// cubo.style.border = "1px solid black";
 		contenedor.style.cssText = `grid-template-columns: repeat(${numeroDeCuadros}, 1fr); grid-template-rows: repeat(${numeroDeCuadros}, 1fr);`;
 		contenedor.appendChild(cubo);
 	}
